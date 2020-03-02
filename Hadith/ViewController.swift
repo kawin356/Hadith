09c09f9193178dc2a9a2efede1d998b9e3512e31
Kittikawin <kawin356@gmail.com>
2020-03-02 22:26:51 +0700

    Initial Commit

---
+++ Hadith/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hadith
+//
+//  Created by Kittikawin Sontinarakul on 2/3/2563 BE.
+//  Copyright © 2563 Kittikawin Sontinarakul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
